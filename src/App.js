153293628c69f2904fb632b8a829b575741e1cf4
Avi-Cohen <aviko.cohen@gmail.com>
2021-01-10 21:51:17 +0200

    added some styling

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ export default function App() {
       <h1>ENG to HEB Flashcards</h1>
       <h2>Click on a card to reaveal the translation</h2>
       <Card frontSide='Hello' backSide='שלום'/>
-      <Card frontSide='Peace' backSide='שלום'/>
       <Card frontSide='World' backSide='עולם'/>
-      <Card frontSide='Goal' backSide='מטרה'/>
+      <Card frontSide='Rainbow' backSide='קשת'/>
       <Card frontSide='Target' backSide='מטרה'/>
+      <Card frontSide='Sun' backSide='שמש'/>
+      <Card frontSide='Moon' backSide='ירח'/>
+      <Card frontSide='Peace' backSide='שלום'/>
+      <Card frontSide='Clock' backSide='שעון'/>
+      <Card frontSide='Goal' backSide='מטרה'/>
+      <Card frontSide='Map' backSide='מפה'/>
+      <Card frontSide='Compass' backSide='מצפן'/>
+      <Card frontSide='Rabi' backSide='רב'/>
     </div>
   );
 }
