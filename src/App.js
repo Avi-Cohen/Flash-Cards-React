98fe77c6a7f596e538174402c6fe41bdc8619c42
Avi-Cohen <aviko.cohen@gmail.com>
2021-01-11 19:56:37 +0200

    organaized App

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React from "react";
 import "./styles.css";
-import Card from "./Card";
-import randomCard from "./RandomCard"
+import FullDictionary from "./FullDictionary"
 import RandomCard from "./RandomCard";
-
-const testData = [
-    { eng: 'Hello', heb: 'שלום' },
-    { eng: 'World', heb: 'עולם' },
-    { eng: 'Rainbow', heb: 'קשת' },
-    { eng: 'Target', heb: 'מטרה' },
-    { eng: 'Sun', heb: 'שמש' },
-    { eng: 'Moon', heb: 'ירח' },
-    { eng: 'Peace', heb: 'שלום' },
-    { eng: 'Clock', heb: 'שעון' },
-    { eng: 'Goal', heb: 'מטרה' },
-    { eng: 'Map', heb: 'מפה' },
-    { eng: 'Compass', heb: 'מצפן' },
-    { eng: 'Rabi', heb: 'רב' }
-]
 
 
 export default function App() {
        return (
             <div className="App">
-                <RandomCard />
                 <h1>ENG to HEB Flashcards</h1>
                 <h2>Click on a card to reaveal the translation</h2>
-                <div>
-                    {testData.map(card => <Card {...card} />)}
-                </div>
-                
-
+                <FullDictionary />
+                <br/>
+                <RandomCard />
             </div>
         );
     }
--- src/FullDictionary.js
+++ src/FullDictionary.js
@@ -3,15 +3,10 @@ import Card from './Card'
 
 export default class RandomCard extends React.Component {
 
-    // let randomCard = () => {
-    //     return testData[Math.floor(Math.random() * testData.length)]
-    // }
     constructor(props) {
         super(props)
-        this.state ={
-            randomNumber: 0,
-            data : [
-                {eng:'?', heb:"!"},
+        this.state = {
+            data: [
                 { eng: 'Hello', heb: 'שלום' },
                 { eng: 'World', heb: 'עולם' },
                 { eng: 'Rainbow', heb: 'קשת' },
@@ -24,19 +19,14 @@ export default class RandomCard extends React.Component {
                 { eng: 'Map', heb: 'מפה' },
                 { eng: 'Compass', heb: 'מצפן' },
                 { eng: 'Rabi', heb: 'רב' }
-            ]}
-
-        }
-        
-        handleButtonClick = () => {
-            this.setState( { randomNumber: Math.floor(Math.random() * 12 + 1)})
+            ]
         }
 
+    }
     render() {
         return (
             <div>
-                <div><Card {...this.state.data[this.state.randomNumber]} /> </div>
-                <button onClick={this.handleButtonClick}>Draw Random Card</button>
+                {this.state.data.map(card => <Card {...card} />)}
             </div>
         );
     }
